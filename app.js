dc22f11b730f8d293c47bcefbf3335a0a2176cc3
Sufyan Chohan <chohansufyan69@gmail.com>
2025-02-26 18:00:55 +0500

    in process

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const data =[
         name:"sufyan",
         desig : "web developer",
         desc : " Hello I am Sufyan Chohan I am Full Stack Developer and What about You",
-        email : "",
-        github :"",
+        email : "Email",
+        github :"Github",
     }
 ]
-function addcard(){
 
-   const cardsSection= document.getElementById("main");
+function adduser(){
+    newuser={
+        name:prompt("write name here"),
+        desig : prompt("write Designation here") ,
+        desc : prompt("write description here"),
+        email : prompt("write email here"),
+        github :prompt("write githublink here"),
+    }
+    data.push(newuser);
+}
+
+
+data.map((item)=>{
+    const cardsSection= document.getElementById("main");
 
     const card= document.createElement("div");
     card.classList.add("card");
@@ -19,25 +31,26 @@ function addcard(){
 
     const cardTitle=document.createElement("h5");
     cardTitle.classList.add("card-title");
-    cardTitle.innerText="Card Title"
+    cardTitle.innerText=item.name
 
     const cardSubtitle=document.createElement("h6");
     cardSubtitle.classList.add("card-subtitle");
-    cardSubtitle.innerText="Card subTitle";
+    cardSubtitle.innerText=item.desig;
 
     const cardText=document.createElement("p");
     cardText.classList.add("card-text");
-    cardText.innerText="Some quick example text to build on the card title and make up the bulk of the card's content.";
+    cardText.innerText=item.desc;
 
     const cardlink1= document.createElement("a");
     cardlink1.classList.add("card-link");
-    cardlink1.innerText="card Link";
-    cardlink1.setAttribute("href","")
+    cardlink1.innerText=item.email;
+    cardlink1.setAttribute("href"," ")
 
     const cardlink2= document.createElement("a");
     cardlink2.classList.add("card-link");
-    cardlink2.innerText="card anoterLink";
-    cardlink1.setAttribute("href","");
+    cardlink2.innerText=item.github;
+    cardlink2.setAttribute("href"," ")
+
 
 
     card.appendChild(cardBody);
@@ -49,5 +62,8 @@ function addcard(){
 
 cardsSection.appendChild(card);
 
-}
-addcard()
+
+})
+
+   
+adduser()
