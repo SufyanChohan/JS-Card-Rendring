2d91116f1daab94ad4ca852b598ebd7d020289cf
Sufyan Chohan <chohansufyan69@gmail.com>
2025-02-27 15:36:23 +0500

    done

--- app.js
+++ app.js
@@ -1,69 +1,78 @@
-const data =[
+const data = [
     {
-        name:"sufyan",
-        desig : "web developer",
-        desc : " Hello I am Sufyan Chohan I am Full Stack Developer and What about You",
-        email : "Email",
-        github :"Github",
+      name: "Sufyan",
+      desig: "Web Developer",
+      desc: "Hello I am Sufyan Chohan. I am a Full Stack Developer. What about you?",
+      email: "Email",
+      github: "Github",
     }
-]
+  ];
+  
+  const cardsSection = document.getElementById("main");
+  
+  function rendercard() {
+    cardsSection.innerHTML = "";
+  
+    data.map((item) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+  
+    
+      const cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+  
+    
+      const cardTitle = document.createElement("h5");
+      cardTitle.classList.add("card-title");
+      cardTitle.innerText = item.name;
+  
+    
+      const cardSubtitle = document.createElement("h6");
+      cardSubtitle.classList.add("card-subtitle");
+      cardSubtitle.innerText = item.desig;
+  
+    
+      const cardText = document.createElement("p");
+      cardText.classList.add("card-text");
+      cardText.innerText = item.desc;
+  
+    
+      const cardLink1 = document.createElement("a");
+      cardLink1.classList.add("card-link");
+      cardLink1.innerText = item.email;
+      cardLink1.setAttribute("href", "#"); 
+  
+    
+      const cardLink2 = document.createElement("a");
+      cardLink2.classList.add("card-link");
+      cardLink2.innerText = item.github;
+      cardLink2.setAttribute("href", "#");
 
-function adduser(){
-    newuser={
-        name:prompt("write name here"),
-        desig : prompt("write Designation here") ,
-        desc : prompt("write description here"),
-        email : prompt("write email here"),
-        github :prompt("write githublink here"),
-    }
+      cardBody.appendChild(cardTitle);
+      cardBody.appendChild(cardSubtitle);
+      cardBody.appendChild(cardText);
+      cardBody.appendChild(cardLink1);
+      cardBody.appendChild(cardLink2);
+  
+      
+      card.appendChild(cardBody);
+  
+     
+      cardsSection.appendChild(card);
+    });
+  }
+  
+  rendercard();
+  
+  function adduser() {
+    const newuser = {
+      name: prompt("Write name here"),
+      desig: prompt("Write Designation here"),
+      desc: prompt("Write description here"),
+      email: prompt("Email Here"),
+      github: prompt("Write githublink here"),
+    };
     data.push(newuser);
-}
-
-
-data.map((item)=>{
-    const cardsSection= document.getElementById("main");
-
-    const card= document.createElement("div");
-    card.classList.add("card");
-
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
-
-    const cardTitle=document.createElement("h5");
-    cardTitle.classList.add("card-title");
-    cardTitle.innerText=item.name
-
-    const cardSubtitle=document.createElement("h6");
-    cardSubtitle.classList.add("card-subtitle");
-    cardSubtitle.innerText=item.desig;
-
-    const cardText=document.createElement("p");
-    cardText.classList.add("card-text");
-    cardText.innerText=item.desc;
-
-    const cardlink1= document.createElement("a");
-    cardlink1.classList.add("card-link");
-    cardlink1.innerText=item.email;
-    cardlink1.setAttribute("href"," ")
-
-    const cardlink2= document.createElement("a");
-    cardlink2.classList.add("card-link");
-    cardlink2.innerText=item.github;
-    cardlink2.setAttribute("href"," ")
-
-
-
-    card.appendChild(cardBody);
-    card.appendChild(cardTitle);
-    card.appendChild(cardSubtitle);
-    card.appendChild(cardText);
-    card.appendChild(cardlink1);
-    card.appendChild(cardlink2);
-
-cardsSection.appendChild(card);
-
-
-})
-
-   
-adduser()
+    rendercard();
+  }
+  
